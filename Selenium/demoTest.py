0b6f3880b5a93c86a49f4bef8a8572eef8d1de9a
yashczoor <42206532+yashczoor@users.noreply.github.com>
2020-08-16 14:18:47 +0200

    added tag name search

--- Selenium/demoTest.py
+++ Selenium/demoTest.py
@@ -5,15 +5,27 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(r"C:\Users\Piotr Jaroszuk\PycharmProjects\SeleniumTutorial\Test.html")
 # driver.maximize_window()
-# driver.find_element_by_id("newPage").click()
+# driver.minimize_window()
 # driver.quit()
 # driver.close() #zamyka okno na którym jest focus
+
+# driver.find_element_by_id("newPage").click()
 driver.find_element_by_id('fname').click()
 # driver.find_element_by_id('tego nie ma') #nie ma to błąd NoSuchElementException
 driver.find_element(By.ID,"fname")
 driver.find_element(By.ID,"clickOnMe")
+
 driver.find_element_by_name('fname')
+
 driver.find_element_by_link_text('Visit W3Schools.com!')
 driver.find_element_by_partial_link_text('W3')
+
 driver.find_element_by_class_name('topSecret') #obiekt może mieć więcej klas. Moge lokalizować po jednej z nich
+
+driver.find_element_by_tag_name("a")
+driver.find_element_by_tag_name("p")
+# driver.find_element_by_tag_name("div") #brak tagu div na stronie testowej
+
+
+
 driver.quit()
